c87ca9d6842fc4b357540ee21ea1e493b7c12790
Ashish Raj Singh <ashishrajsingh75@gmail.com>
2026-06-12 20:11:19 +0530

    style: update Dock component styles; adjust button layout and icon size for improved UI

--- src/UI/Dock.tsx
+++ src/UI/Dock.tsx
@@ -13,15 +13,15 @@ const Dock = () => {
     ];
 
     return (
-        <footer className="z-20 h-16 w-full  flex items-center justify-center gap-4 px-4 pb-2">
+        <footer className="z-20 h-16 w-full  flex items-center justify-center gap-5 px-4 pb-2">
             {apps.map(app => (
                 <button 
                     key={app.id}
                     onClick={() => dispatch(executeProcess(app.id))} 
-                    className="flex flex-col items-center justify-center w-12 h-12 bg-white/10 rounded-xl hover:bg-white/10 hover:-translate-y-1 transition-all duration-200 border border-white/10 hover:border-white/20 shadow-lg"
+                    className="flex flex-col items-center justify-center w-12 h-12 transform  scale-100  hover:-translate-y-1 hover:scale-105 transition-all duration-200"
                     title={app.name}
                 >
-                    <span className="text-2xl drop-shadow-md">{app.icon}</span>
+                    <span className="text-5xl">{app.icon}</span>
                 </button>
             ))}
         </footer>
